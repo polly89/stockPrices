741ae5a7410d77192ec59da732835e6e59f91d21
Christina Woodhams <cwoodhams89@gmail.com>
2022-09-21 18:20:38 -0400

    Second commit. Modification shows the day you should buy and sell.

--- stockPrices.js
+++ stockPrices.js
@@ -1,18 +1,25 @@
-function best(stocks){
-    let largestDiff = 0;
-    let smallestNumSeen = stocks[0];
+function best(price, n){
+if (n == 1)
+    return;
 
-    for(let i = 1; i < stocks.length; i++){
-        const currentPrice = stocks[i];
-        const currentDiff = currentPrice - smallestNumSeen;
-        if(currentPrice < smallestNumSeen){
-            smallestNumSeen = currentPrice;
-        }else if(currentDiff >largestDiff){
-            largestDiff = currentDiff;
-        }
+    let i = 0;
+    while (i < n - 1){
+        while ((i < n - 1) && (price[i + 1] <= price[i]))
+        i++;
+        if(i == n - 1)
+            break;
+
+            let buy = i++;
+            while ((i < n) && (price[i] >= price[i - 1]))
+            i++;
+            let sell = i - 1;
+
+            console.log(`Buy on day: ${buy}. Sell on day: ${sell} <br>`);
     }
-    return largestDiff
+
 }
 
-test = [100, 180, 260, 310, 40, 535, 695]
-console.log(best(test))
+let price = [100, 180, 260, 310, 40, 535, 695];
+let n = price.length;
+
+best(price, n);
